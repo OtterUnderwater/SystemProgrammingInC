635da96078dedc3af5e81e4d0c348fc9e694aace
unknown <@ngknn.local>
2024-10-14 17:37:53 +0300

    fix: #if #endif for PreprocessorDirectives

--- PreprocessorDirectives/PreprocessorDirectives/Source.c
+++ PreprocessorDirectives/PreprocessorDirectives/Source.c
@@ -1,11 +1,11 @@
 #define _CRT_SECURE_NO_WARNINGS
 #define POW(x) (x)*(x) //макрос возводит число во вторую степень
-#define X1 0
-#define Y1 0
-#define X2 0
-#define Y2 0
-#define X3 0
-#define Y3 0
+#define X1 1
+#define Y1 2
+#define X2 3
+#define Y2 4
+#define X3 5
+#define Y3 6
 #define A sqrt(POW(X3 - X1) + POW(Y3 - Y1))
 #define B sqrt(POW(X3 - X2) + POW(Y3 - Y2))
 #define C sqrt(POW(X2 - X1) + POW(Y2 - Y1))
@@ -33,9 +33,8 @@ float GetLengthSegment(float x1, float y1, float x2, float y2)
 /// <param name="y1"></param>
 /// <param name="x2"></param>
 /// <param name="y2"></param>
-void GetPerimeterAndSquare(float x1, float y1, float x2, float y2)
+void GetPerimeterAndSquare(float x1, float y1, float x2, float y2, float x3, float y3)
 {
-    float x3 = x2, y3 = y1;
     float a = GetLengthSegment(x1, y1, x3, y3);
     float b = GetLengthSegment(x2, y2, x3, y3);
     float c = GetLengthSegment(x1, y1, x2, y2);
@@ -46,12 +45,12 @@ void GetPerimeterAndSquare(float x1, float y1, float x2, float y2)
         float S = sqrt(p * (p - a) * (p - b) * (p - c));
         printf("Треугольник существует, периметр: %.2f, площадь: %.2f\n", P, S);
     }
-    else printf("Треугольник не существует");
+    else printf("Треугольник не существует\n");
 }
 
 void GetPerimeterAndSquare2()
 {
-#if ((X2 - X1) * (Y2 - Y1) == (X3 - X1)* (Y3 - Y1))
+#if ((X2 - X1) / ((X3 - X1) == 0 ? 1:(X3 - X1))) == ((Y2 - Y1) / ((Y3 - Y1) == 0 ? 1:(Y3 - Y1)))
     printf("Треугольник не существует\n");
 #else
     float P = A + B + C;
@@ -64,7 +63,7 @@ void GetPerimeterAndSquare2()
 int main()
 {
     system("chcp 1251>nul");
-   //GetPerimeterAndSquare(1, 2, 3, 5);
+    GetPerimeterAndSquare(1, 2, 3, 4, 5, 6);
     GetPerimeterAndSquare2();
     return 0;
 }
